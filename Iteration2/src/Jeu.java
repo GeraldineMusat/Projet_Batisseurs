927c4af6dc69d8ee4e402545cd92acb52eec11f0
Unknown <sebsaez1007@hotmail.fr>
2018-02-26 16:23:53 +0100

    Class Jeu
     - methodes à completer

--- Iteration2/src/Jeu.java
+++ Iteration2/src/Jeu.java
@@ -2,18 +2,44 @@ import java.util.ArrayList;
 
 public abstract class Jeu {
 
-    private ArrayList reserve = new ArrayList(); //Pieces a disposition
-    private ArrayList paquetOuvrier = new ArrayList(); //Taille 42
-    private ArrayList listCarteOuvrierSelectionnable = new ArrayList(); // Taille MAX 5
-    private ArrayList paquetBatiment = new ArrayList(); // Taille  42
-    private ArrayList ListCarteBatimentSelectionnable= new ArrayList(); // Taille MAX 5
-    private ArrayList listJoueur = new ArrayList(); // Taille MAX 4
+    static ArrayList reserve = new ArrayList(); //Pieces a disposition
+    static ArrayList paquetOuvrier = new ArrayList(); //Taille 42
+    static ArrayList listCarteOuvrierSelectionnable = new ArrayList(); // Taille MAX 5
+    static ArrayList paquetBatiment = new ArrayList(); // Taille  42
+    static ArrayList ListCarteBatimentSelectionnable= new ArrayList(); // Taille MAX 5
+    static ArrayList listJoueur = new ArrayList(); // Taille MAX 4
 
-    public static void main(){
+
+
+    static void genererPiece(){
 
     }
 
-    
+    static void genererPaquetOuvrier(){
+
+    }
+
+    static void genererPaquetBatiment(){
+
+    }
+
+    static void addJoueur(){
+
+    }
+
+    static void setListCarteOuvrierSelectionnable(){
+
+    }
+
+    static void setListCarteBatimentSelectionnable(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Les batisseurs");
+    }
+
+
 }
 
 
