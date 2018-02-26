794a1ecd04f98a2553af22911ea410e23bfbbab4
Unknown <sebsaez1007@hotmail.fr>
2018-02-26 17:18:16 +0100

    Update Squelette

--- Iteration2/src/Carte.java
+++ Iteration2/src/Carte.java
@@ -1,2 +1,17 @@
-public class Carte {
+public abstract class Carte {
+    protected int ecu = 0; // a recevoir ou a payer
+    protected int ressourcesPierre;
+    protected int ressourcesBois;
+    protected int ressourcesSavoir;
+    protected int ressourcesTuile;
+    protected String nom = "";
+
+    public Carte(String nom, int rp, int rb, int rs, int rt, int ecu){
+        this.nom = nom;
+        this.ressourcesPierre =rp;
+        this.ressourcesBois = rb;
+        this.ressourcesSavoir = rs;
+        this.ressourcesTuile = rt;
+        this.ecu = ecu;
+    }
 }
--- Iteration2/src/Ouvrier.java
+++ Iteration2/src/Ouvrier.java
@@ -1,2 +1,14 @@
-public class Ouvrier {
+public class Ouvrier extends Carte{
+    private boolean estEnTrainDeTravailler = false;
+
+    public Ouvrier(String nom, int rp, int rb, int rs, int rt, int ecu){
+        super(nom,rp,rb,rs,rt,ecu);
+    }
+
+    public void setEstEnTrainDeTravailler(boolean bool) {
+        this.estEnTrainDeTravailler = bool;
+    }
+    public boolean getEstEnTrainDeTravailler(){
+        return this.estEnTrainDeTravailler;
+    }
 }
