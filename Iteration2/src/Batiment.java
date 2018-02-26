94019683de3493ba6fa3996880e53bf52fee4e74
Unknown <sebsaez1007@hotmail.fr>
2018-02-26 18:09:16 +0100

    Squelette batiment

--- Iteration2/src/Batiment.java
+++ Iteration2/src/Batiment.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class Batiment {
+
+    private int pointsVictoire = 0;
+    private ArrayList<Ouvrier> listOuvrierTravaillant = new ArrayList<Ouvrier>();
+    private boolean estConstruit = false;
+
+    public int getPointsVictoire() {
+        return pointsVictoire;
+    }
+
+    public boolean isEstConstruit() {
+        return estConstruit;
+    }
+
+    public void setEstConstruit(boolean estConstruit) {
+        this.estConstruit = estConstruit;
+    }
+
+    public void ajouterUnOuvrier(Ouvrier o) throws Throwable {
+        if(o.getEstEnTrainDeTravailler()){
+            this.listOuvrierTravaillant.add(o);
+        }else{
+            throw new Throwable("Ouvrier travaillant deja sur un autre batiment");
+        }
+
+    }
 }
