e4f808ae621780979ef1c5fb37bb8b4cea61fb1a
Unknown <sebsaez1007@hotmail.fr>
2018-02-26 15:54:12 +0100

    Création des class

---
+++ Iteration2/src/Jeu.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public abstract class Jeu {
+
+    private ArrayList reserve = new ArrayList(); //Pieces a disposition
+    private ArrayList paquetOuvrier = new ArrayList(); //Taille 42
+    private ArrayList listCarteOuvrierSelectionnable = new ArrayList(); // Taille MAX 5
+    private ArrayList paquetBatiment = new ArrayList(); // Taille  42
+    private ArrayList ListCarteBatimentSelectionnable= new ArrayList(); // Taille MAX 5
+    private ArrayList listJoueur = new ArrayList(); // Taille MAX 4
+
+    public static void main(){
+
+    }
+
+}
+
+
+
